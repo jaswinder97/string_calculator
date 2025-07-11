5b8d83890a0b7c185f37dabb62b02f638ceba7ce
jaswinder97 <jaswindersinghsn97+1@gmail.com>
2025-07-11 21:30:03 +0530

    Fix rubocop offences

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -7,23 +7,23 @@ class Calculator
     numbers_part = input
     delimiter_pattern = /,|\n/ # default delimiters
 
-    if input.start_with?("//")
+    if input.start_with?('//')
       header, numbers_part = input.split("\n", 2)
 
-      if header.include?("[")
-        # Multiple or multi-character delimiters like //[***][@@]
-        delimiters = header.scan(/\[([^\]]+)\]/).flatten
-      else
-        # Single-character custom delimiter like //;
-        delimiters = [header[2]]
-      end
+      delimiters = if header.include?('[')
+                     # Multiple or multi-character delimiters like //[***][@@]
+                     header.scan(/\[([^\]]+)\]/).flatten
+                   else
+                     # Single-character custom delimiter like //;
+                     [header[2]]
+                   end
 
       delimiter_pattern = Regexp.union(delimiters)
     end
 
     numbers = numbers_part.split(delimiter_pattern).map(&:to_i)
 
-    negatives = numbers.select { |n| n < 0 }
+    negatives = numbers.select(&:negative?)
     raise "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
 
     numbers.sum
